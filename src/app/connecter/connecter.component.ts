52f1869bb9be4307f6f63e7bace32da1b469cc88
linlin-Z <lin.zhong.contact@gmail.com>
2020-02-25 11:01:50 +0100

    2 files modified

--- src/app/connecter/connecter.component.ts
+++ src/app/connecter/connecter.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+
 @Component({
   selector: 'app-connecter',
   templateUrl: './connecter.component.html',
@@ -13,4 +14,22 @@ export class ConnecterComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export class LoginFormComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({  // Crée une instance de FormGroup
+      username: ['linlin']                 // Crée une instance de FormControl
+  
+    });
+  }
+
+  login() {
+    console.log('Données du formulaire...', this.loginForm.value);
+     console.log(this.loginForm.value);
+  }
 }
