089605212169c40d1c2a1a0ffe2412d18d8d5b3a
linlin-Z <lin.zhong.contact@gmail.com>
2020-02-28 12:15:36 +0100

    1 file modified

--- src/app/connecter/ajoute/ajoute.component.ts
+++ src/app/connecter/ajoute/ajoute.component.ts
@@ -10,15 +10,26 @@ import { Component, OnInit } from '@angular/core';
       (blur)="addSite(newSite.value); newSite.value='' ">
       <button (click)=addSite(newSite.value)>Add</button>
     <ul><li *ngFor="let site of sites">{{site}}</li></ul>
+    <button (click)="clicklike()">like!</button>
+    {{compteurlike}}
+    <button (click)="clickunlike()">unlike!</button>
+    {{compteurunlike}}
   `
 })
 export class AjouteComponent implements OnInit {
-
+compteurlike=0;
+compteurunlike=0;
    sites = [ 'Google', 'Taobao', 'Facebook'];
   addSite(newSite: string) {
     if (newSite) {
       this.sites.push(newSite);
     }
+  }
+  clicklike(){
+    this.compteurlike++;
+  }
+  clickunlike(){
+    this.compteurunlike++;
   }
 
 }
