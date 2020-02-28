a51f22abb1fd77fc8ca40b9317b9d9e79da80568
linlin-Z <lin.zhong.contact@gmail.com>
2020-02-28 10:31:56 +0100

    3 files modified

--- src/app/connecter/ajoute/ajoute.component.ts
+++ src/app/connecter/ajoute/ajoute.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ajoute',
   templateUrl: './ajoute.component.html',
-  styleUrls: ['./ajoute.component.css']
+  styleUrls: ['./ajoute.component.css'],
+    template: `
+    <input #newSite
+      (keyup.enter)="addSite(newSite.value)"
+      (blur)="addSite(newSite.value); newSite.value='' ">
+      <button (click)=addSite(newSite.value)>Add</button>
+    <ul><li *ngFor="let site of sites">{{site}}</li></ul>
+  `
 })
 export class AjouteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+   sites = [ 'Google', 'Taobao', 'Facebook'];
+  addSite(newSite: string) {
+    if (newSite) {
+      this.sites.push(newSite);
+    }
   }
 
 }
